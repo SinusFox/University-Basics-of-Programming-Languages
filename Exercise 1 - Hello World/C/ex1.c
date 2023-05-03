27a6f71521252cd2ee2304a9aa0d7674fbca232c
SinusFox <sinusfox@pm.me>
2023-05-03 18:30:26 +0200

    upload of Exercise 1

---
+++ Exercise 1 - Hello World/C/ex1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World the second - I coded this before. So yeah, FOXES ARE SUPERIOR.\nBut the professor wants us to write the following: \"index = 2 * count + 42;\"");
+    return 0;
+}
