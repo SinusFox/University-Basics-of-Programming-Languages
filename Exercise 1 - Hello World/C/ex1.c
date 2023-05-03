53fec3a674fe068a0a7ab0126f038eabe9f44b8a
SinusFox <sinusfox@pm.me>
2023-05-03 18:55:17 +0200

    extracting the text to a var

--- Exercise 1 - Hello World/C/ex1.c
+++ Exercise 1 - Hello World/C/ex1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World the second - I coded this before. So yeah, FOXES ARE SUPERIOR.\nBut the professor wants us to write the following: \"index = 2 * count + 42;\"");
+    char* sourcecode = "Hello World the second - I coded this before. So yeah, FOXES ARE SUPERIOR.\nBut the professor wants us to write the following: \"index = 2 * count + 42;\"";
+    printf(sourcecode);
     return 0;
 }
