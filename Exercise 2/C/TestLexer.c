9cde0cf1dfaa1c4230347fd20ee5d640f832584c
SinusFox <sinusfox@pm.me>
2023-05-09 15:25:14 +0200

    moved lexer functions to Lexer.c and included Lexer.c

--- Exercise 2/C/TestLexer.c
+++ Exercise 2/C/TestLexer.c
@@ -1,35 +1,6 @@
 #include <stdio.h>
 #include "Lexer.h"
-
-int start(char* sourcecode) {
-    // if (sourcecode != NULL) {
-    //     return _GET_NEXT_CHARACTER_;
-    // } else {
-    //     return _STOP_;
-    // }
-
-    return _STOP_; // placeholder
-}
-
-int getNextCharacter() {
-    return _STOP_; // placeholder
-}
-
-int readIdentifier() {
-    return _STOP_; // placeholder
-}
-
-int readNumLit() {
-    return _STOP_; // placeholder
-}
-
-int readOperator() {
-    return _STOP_; // placeholder
-}
-
-int readPunctuation() {
-    return _STOP_; // placeholder
-}
+#include "Lexer.c"
 
 int main() {
     char* sourcecode = "Hello World the second - I coded this before. So yeah, FOXES ARE SUPERIOR.\nBut the professor wants us to write the following: \"index = 2 * count + 42;\"\n\n";
@@ -60,7 +31,7 @@ int main() {
         }
     } while (lexer_state != _STOP_);
 
-    printf("Lexer exited with code ");
+    // printf("Lexer exited with code ");
     // printf(lexer_state);
 
     return 0;
